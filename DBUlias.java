115c533f6c6af36aaa06fcf85c31290d0d125c9e
nagarajapl <nagaraja.plokesh@gmail.com>
2022-04-24 22:44:47 +0530

    conflict

--- DBUlias.java
+++ DBUlias.java
@@ -2,3 +2,4 @@ hi iam testing and working on branchins and wp
 this is the file contains db related codej
 hi iam devoloping code for thr day
 iam testing reset command
+iam testing merging conflict
