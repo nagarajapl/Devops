1f6d72a6f487a71cc5efe295974a0715bad62b6a
nagarajapl <nagaraja.plokesh@gmail.com>
2022-04-25 06:14:36 +0530

    commit

--- DBUlias.java
+++ DBUlias.java
@@ -1,5 +1,5 @@
 hi iam testing and working on branchins and wp
-this is the file contains db related codej
-hi iam devoloping code for thr day
-iam testing reset command
-iam testing merging conflict
+hello we rae the same page
+dont burn your bridges
+Ima learning devops
+iam testing git merging
