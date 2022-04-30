50deb9661ed7f3f17aa2cbb4eced60dd7749d3d5
nagarajapl <nagaraja.plokesh@gmail.com>
2022-04-30 07:25:14 +0530

    fixed

--- DBUlias.java
+++ DBUlias.java
@@ -1,9 +1,5 @@
 hi iam testing and working on branchins 
 this is the file contains db related codej
-hi iam devoloping code for thhjasbdhjbahfkb
-iam testing reset command
-iam testing merging confct
-hello we rae the same page
 dont burn your bridges
 Ima learning devops
 iam testing git merging
